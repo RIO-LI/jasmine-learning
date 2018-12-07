e87a36468272918ab57c466a5a1f5b117f6309c6
1406081008@qq.com <cjt19930809>
2018-12-07 10:49:53 +0800

    update log: 1.添加lesson单元js文件的一些解释注释

--- lesson04/spec/hk-paparazzo.spec.js
+++ lesson04/spec/hk-paparazzo.spec.js
@@ -1,4 +1,9 @@
 const { Paparazzo } = require("hk-paparazzo");
+/**
+ * 只有这个测试单元会被jasmine调用，因为在npm script中配置了
+ * --filter='only hk-paparazzo.spec.js file will be tested'
+ * 与 filter 指定的字符串不同的测试单元都不会被调用
+ */
 describe("only hk-paparazzo.spec.js file will be tested", function () {
     it("hk-paparazzo library test", function () {
         expect(typeof new Paparazzo()).toBe('object');
